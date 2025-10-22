c459f0c06e71940431c9a41ac3c8d23a61837a4f
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-10-22 15:59:11 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import turtle
+
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+background_image = "us_states_game/blank_states_img.gif"
+screen.addshape(background_image)
+
+
+turtle = turtle.Turtle()
+turtle.shape(background_image)
+
+
+
+screen.exitonclick()
