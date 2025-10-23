5696b68f3d597b5989b61bd05c750ee6a5e7b66b
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-10-23 21:34:41 +0300

    updating some variables name

--- main.py
+++ main.py
@@ -8,68 +8,33 @@ screen.addshape(background_image) # to make it available for screen.
 turtle.shape(background_image)
 
 
-NUM_OF_CORRECT_STATES = 0
+num_of_correct_guesses = 0
 all_data = pd.read_csv("us_states_game/50_states.csv")
 states_list = all_data.state.to_list()  # convert to normal Python list
-ALL_STATES = len(states_list)
-CORRECT_GUESS = []
+all_states = len(states_list)
+correct_guessed_list = []
 
 
 
 game_is_on = True
 while game_is_on:
-    user_guess = screen.textinput(title=f"{NUM_OF_CORRECT_STATES}/{ALL_STATES} States Correct", prompt="What's another state name?").title()
+    user_guess = screen.textinput(title=f"{num_of_correct_guesses}/{all_states} States Correct", prompt="What's another state name?").title()
     
     if user_guess in states_list:
-        if user_guess in CORRECT_GUESS:
+        if user_guess in correct_guessed_list:
             continue
         guess_coor_row = all_data[all_data.state == user_guess]
-        x_coor = float(guess_coor_row.x)
+        x_coor = float(guess_coor_row.x) # we have to convert the coor because the value in csv files are always str.
         y_coor = float(guess_coor_row.y)
         new_state = turtle.Turtle()
         new_state.penup()
         new_state.hideturtle()
         new_state.goto(x_coor, y_coor)
         new_state.write(user_guess)
-        NUM_OF_CORRECT_STATES += 1
-        CORRECT_GUESS.append(user_guess)
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        num_of_correct_guesses += 1
+        correct_guessed_list.append(user_guess)
+        if correct_guessed_list == states_list:
+            game_is_on = False
 
 
 screen.exitonclick()
