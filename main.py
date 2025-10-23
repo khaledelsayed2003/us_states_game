33673b004766ec733e5126635838a474d5b8f79c
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-10-23 19:19:48 +0300

    Read csv file and getting the state column using pandas

--- main.py
+++ main.py
@@ -1,15 +1,69 @@
 import turtle
-
+import pandas as pd
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 background_image = "us_states_game/blank_states_img.gif"
-screen.addshape(background_image)
-
-
-turtle = turtle.Turtle()
+screen.addshape(background_image) # to make it available for screen.
 turtle.shape(background_image)
+
+
+NUM_OF_CORRECT_STATES = 0
+all_data = pd.read_csv("us_states_game/50_states.csv")
+states_list = all_data.state
+ALL_STATES = len(states_list)
+
+
+
+user_guess = screen.textinput(title=f"{NUM_OF_CORRECT_STATES}/{ALL_STATES} States Correct", prompt="What's another state name?")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 screen.exitonclick()
+
+
+
+
+
+
+
